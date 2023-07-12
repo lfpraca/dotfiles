0844345a7b1d071005be9b61b530edfd48c05ca8
lfpraca <luisfelippe@protonmail.com>
2023-07-12 18:18:42 -0300

    Adds xorg keybaord config

--- copy_from.sh
+++ copy_from.sh
@@ -6,3 +6,4 @@ cp $HOME/.emacs.d/init.el emacs/init.el
 cp $HOME/.config/i3/config i3/config
 cp $HOME/.config/polybar/config.ini polybar/config.ini
 cp $HOME/.config/polybar/launch.sh polybar/launch.sh
+cp /etc/X11/xorg.conf.d/00-keyboard.conf xorg/00-keyboard.conf
